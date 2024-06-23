602808b9087f6ca7dc45f24bdaee9aff273fdb92
raulsync <duffer.rdx@gmail.com>
2024-06-23 14:21:54 +0530

    navbar-completed

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,12 +1,38 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../ui/input';
 import logo from './../../assets/logo.png';
+
 const Navbar = () => {
   return (
-    <div className="flex justify-between gap-3">
+    <div className="flex justify-between items-center md:px-10 sm:px-5 px-5 my-3 text-xl gap-3">
       <img
-        className=" h-10 px-9 sm:px-5 md:px-5"
+        className=" h-9 md:h-14 sm:h-9 hover:opacity-70 cursor-pointer"
         src={logo}
         alt="logo"
       />
+
+      <div className=" flex gap-3 items-center ">
+        <form action="">
+          <Input
+            type="text"
+            placeholder="search"
+            className=" border-gray-400 rounded-2xl md:w-fit sm:w-[30vw]  outline-none shadow-slate-700"
+          />
+        </form>
+        <div className=" md:block sm:hidden">
+          <div className="flex gap-6 items-center">
+            <Link to={'/action'}>
+              <div>Action</div>
+            </Link>
+            <Link to={'/movies'}>
+              <div>Movies</div>
+            </Link>
+            <Link to={'/shows'}>
+              <div>TvShows</div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
